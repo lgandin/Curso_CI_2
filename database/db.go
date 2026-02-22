40da7ade09ac850fe6056aa33ade2986df30e4aa
Leonardo Gandin <62913908+lgandin@users.noreply.github.com>
2026-02-22 12:54:23 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -18,7 +18,7 @@ var (
 func ConectaComBancoDeDados() {
 
 	stringDeConexao := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
