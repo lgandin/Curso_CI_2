f90ac817e96fa1961c5c78f377a9f7e38dce5e0c
Leonardo Gandin <62913908+lgandin@users.noreply.github.com>
2026-02-22 13:06:47 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -23,13 +23,15 @@ func ConectaComBancoDeDados() {
     }
 	
 	stringDeConexao := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+    "host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+    os.Getenv("DB_HOST"),
+    os.Getenv("DB_USER"),
+    os.Getenv("DB_PASSWORD"),
+    os.Getenv("DB_NAME"),
+    os.Getenv("DB_PORT"),
+    sslMode,
+)
+
 
 	DB, err = gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
 	if err != nil {
